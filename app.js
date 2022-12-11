697297b844ea003a4386cb4bfd06d5ddf6089a8f
Ayodele Abraham <ayodeled550@gmail.com>
2022-12-11 10:51:06 +0100

    modified a few lines

--- app.js
+++ app.js
@@ -1,82 +1,42 @@
 
 function getComputerChoice() {
-    let computerSelection = ['rock', 'scissors', 'paper']
-    return computerSelection[Math.floor(Math.random() * computerSelection.length)]
+    let computerSelection = ['rock', 'scissors', 'paper'];
+    return computerSelection[Math.floor(Math.random() * computerSelection.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    if(computerSelection == playerSelection) {
+    if(computerSelection === playerSelection) {
         return "Draw";
-    }else if(computerSelection == 'rock'){
-        if(playerSelection == "paper"){
+    }else if(computerSelection === 'rock'){
+        if(playerSelection === "paper"){
             return "You Won! paper beats rock";
-        }else if(playerSelection == "scissors"){
+        }else if(playerSelection === "scissors"){
             return "You Lost! rock beats scissors";
         }
-    }else if(computerSelection == 'scissors'){
-        if(playerSelection == 'paper') {
+    }else if(computerSelection === 'scissors'){
+        if(playerSelection === 'paper') {
             return "You Lost! scissors beats paper";
-        }else if(playerSelection == 'rock'){
+        }else if(playerSelection === 'rock'){
             return "You Won! rock beats scissors";
         }
-    }else if(computerSelection == 'rock'){
-        if(playerSelection == 'paper') {
+    }else if(computerSelection === 'rock'){
+        if(playerSelection === 'paper') {
             return "You Won! paper beats rock";
-        }else if(playerSelection == 'scissors'){
-            return "You Lost! rock beats scissors"
+        }else if(playerSelection === 'scissors'){
+            return "You Lost! rock beats scissors";
         }
     }
 }  
 
 const computerSelection = getComputerChoice();
 const playerSelection = prompt().toLocaleLowerCase();
-console.log(computerSelection,playerSelection);
-console.log()
 
-/*
-let playerSelectFrom = prompt();
-if computerSelectFrom == playerSelectFrom {
-    console.log('Draw');
-}else if computerSelectFrom {
-
-}
+//for(let i = 0; i < 5; i++){
+  //  function game(){
+    //    return playRound(playerSelection, computerSelection);
+    //}
+//}
+//console.log(game())
 
 
-    if(computerSelectFrom === playerSelectFrom) {
-        return "You draw"
-    } else if(computerSelectFrom === 'rock' && playerSelectFrom === 'scissors') {
-        return "You Lose! Rock beats Scissors";
-    }else if(computerSelectFrom === 'Scissors' && playerSelectFrom === 'Rock') {
-        return "You win! Rock beats Scissors";
-    } else if(computerSelectFrom === 'paper' && playerSelectFrom === 'scissors') {
-        return "You win! Scissors beats paper";
-    } else if(computerSelectFrom === 'rock' && playerSelectFrom === 'paper') {
-        return "You Win! Paper beat rock";
-    } else if(computerSelectFrom === 'scissors' && playerSelectFrom === 'paper') {
-        return "You Lose! Scissors beats Paper";
-    } else if(computerSelectFrom === 'paper' && playerSelectFrom === 'rock') {
-        return "You Lose! Paper beats rock";
-    }
-        
-
-
-const playerSelectFrom = ["rock", "scissors", "paper"];
-computerSelectFrom = getComputerChoice();
-console.log(playRound(computerSelectFrom, playerSelectFrom)); 
-
-function game() {
-    for(let i = 0; i < 5; i++) {
-        return playRound()
-    }
-}
-console.log(game())
-
-
-
-
-
-
-
-console.log(getComputerChoice());
-
-*/
+console.log(playRound(playerSelection, computerSelection));
